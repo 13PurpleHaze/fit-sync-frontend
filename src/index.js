b044bd2974776f41150706499df68814943f5c1b
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2023-06-23 18:29:42 +0500

    feat: add login page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import NavBar from "./components/NavBar";
 import reportWebVitals from './reportWebVitals';
+import Login from "./pages/Login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar/>
+    <Login/>
   </React.StrictMode>
 );
 
